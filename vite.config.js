a7b6ac6073edc82c5457cb58639dcb54afc8b125
Abhishek <devilizbusy@gmail.com>
2024-10-18 10:48:05 +0530

    Update package files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+<<<<<<< HEAD
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+=======
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+})
+>>>>>>> 8261e38 (Normalize line endings)
